79cdc3369219d9597bec09dc6f3de1bcc3a0eae4
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-08-31 15:33:21 -0300

    @

--- Algoritmo e Programação/Atividade 2/5.cpp
+++ Algoritmo e Programação/Atividade 2/5.cpp
@@ -2,18 +2,27 @@
 #include <stdio.h>    
 #include <stdlib.h>
 #include <locale.h>
-#include <cmath>
-
 int main(void)
 { 
-	int num;
-	
-	printf("Informe o número: \n");
-	scanf("%d",&num);
-	
-	if (num >= 100 && num <= 999)
-	{
-		printf("Você informou o número %d",num);
-	} else 
-		printf("Número inteiro inválido \n");
+	int numero;
+	int soma;
+	printf("\n Informe um número entre 100 a 999:");
+	scanf("%d",&numero);
+	if(numero >= 100 && numero <= 999){
+	// Exemplo - Entrada de 415
+	while( numero > 0 )
+    {
+    	// Usar o operador aritmético de módulo % 
+        soma = soma + (numero % 10);
+        // 415 % 10 = 5
+        // 41 % 1 = 1
+        // 4 % 1 = 4
+        numero = numero / 10;
+        // 415 / 10 = 41
+        // 41 / 10 = 4
+        // 4 / 10 = 0.4
+    }
+	printf("\n A soma dos digitos das casas do número %d é %d", numero, soma);	
+	} else
+		printf("\n O número informado está inválido");
 }
