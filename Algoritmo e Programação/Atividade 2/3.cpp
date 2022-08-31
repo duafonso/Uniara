da9e0b4158a4aa276dc1b73289dfa94f674a34c8
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-08-31 15:40:45 -0300

    @

--- Algoritmo e Programação/Atividade 2/3.cpp
+++ Algoritmo e Programação/Atividade 2/3.cpp
@@ -1,20 +1,16 @@
 #include <stdio.h>    
 #include <stdlib.h>
 #include <locale.h>
-
 int main(void)
 { 
-	float salario = 1800;
-	float valor_carro = 75;
+	int salario = 1800;
+	int valor_carro = 75;
 	int quantidade;
-	
 	printf("Informe a quantidade de carros vendidos: \n");
 	scanf("%d",&quantidade);
-	
-	float vendas = (quantidade * valor_carro);
-	float comissao = vendas / 
-	float salario_final = salario + vendas + comissao;
-	
+	int vendas = (quantidade * valor_carro);
+	int comissao = vendas * 0.05; 
+	int salario_final = salario + vendas + comissao;
 	printf("\n Foram vendidos %d carros", quantidade);
-	printf("\n O salario final será de %f", salario_final);
+	printf("\n O salario final será de %d", salario_final);
 }
