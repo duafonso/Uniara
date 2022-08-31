3ad500d8871817418fd91c3f3118e62a3d2d4b2e
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-08-31 20:35:05 -0300

    @

---
+++ Algoritmo e Programação/Atividade 1/10.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>    
+#include <stdlib.h>
+#include <locale.h>
+int main(void)
+{ 
+    //10. Escreva um algoritmo que leia 3 números inteiros 
+    //e mostre o maior deles 
+    int n1, n2, n3;
+    setlocale(LC_ALL, "Portuguese");
+    printf("Informe o valor do número 1: \n");
+    scanf("%d",&n1);
+    printf("Informe o valor do número 2: \n");
+    scanf("%d",&n2);
+    printf("Informe o valor do número 3: \n");
+    scanf("%d",&n3);
+    if(n1 >= n2 and n1 >= n3)
+    printf("\n O número 1 possui o valor %d e assim é o maior",n1);
+    if(n2 >= n1 and n2 >= n3)
+    printf("\n O número 2 possui o valor %d e assim é o maior",n2);  
+    if(n3 >= n1 and n1 >= 2)
+    printf("\n O número 3 possui o valor %d e assim é o maior",n3);
+}
