26fa295b451c899b6d2e014ca4ba38bea83aff44
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-08-31 21:03:43 -0300

    @

--- Algoritmo e Programação/Atividade 2/1.cpp
+++ Algoritmo e Programação/Atividade 2/1.cpp
@@ -1,43 +1,24 @@
-// 1. Considerando A, B, C e D como variáveis inteiras, 
-// crie um programa em Linguagem C para calcular o valor de X
-
 #include <stdio.h>    
 #include <stdlib.h>
 #include <locale.h>
 
 int main(void)
 { 
-	// Declaração das Variáveis em Tipo Inteiro ( Utilizando // para ser Comentário)
-	int A, B, C, D, X;
-	// Configurando para acentuação com o idioma português ( Utilizando // para ser Comentário)
+    int A, B, C, D;
 	setlocale(LC_ALL, "Portuguese");
-
-		// printf exibirá a mensagem no prompt / terminal para o usuário
-		printf("Informe o valor da variável A: ");
-		scanf("%d", &A);
-		// %d é referente a números decimais inteiros e &A é a váriavel que vai receber 
-		
-		// printf exibirá a mensagem no prompt / terminal para o usuário
-		printf("Informe o valor da variável B: ");
-		scanf("%d", &B);
-		// %d é referente a números decimais inteiros e &A é a váriavel que vai receber 
-		
-		// printf exibirá a mensagem no prompt / terminal para o usuário
-		printf("Informe o valor da variável C: ");
-		scanf("%d", &C);
-		// %d é referente a números decimais inteiros e &A é a váriavel que vai receber 
-		
-		// printf exibirá a mensagem no prompt / terminal para o usuário
-		printf("Informe o valor da variável D: ");
-		scanf("%d", &D);
-		// %d é referente a números decimais inteiros e &A é a váriavel que vai receber 
-
-	// Execução do Cálculo do Resultado
-	// Tentar utilizar X = ((B / A + C) + (4 * A)) / ((D - 2 * A) / ( 3 + C ))
-	X = A + B;
-
-	// Apresentação da Variável Resultado em X
-	printf("O valor da variável X é %d ", X);
-	
-	return 0;
+	printf("Informe o valor da variável A: ");
+	scanf("%d", &A);
+	printf("Informe o valor da variável B: ");
+	scanf("%d", &B);
+	printf("Informe o valor da variável C: ");
+	scanf("%d", &C);
+    printf("Informe o valor da variável D: ");
+	scanf("%d", &D);
+    int A_1 = (B / (A + C)) + (4 * A);
+    int A_2 = (D - (2 * A)) / 3 + C;
+    int X_1 = A_1 / A_2;
+    int B_1 = (-1 * B) - (B * B * B) - (4 * A * C) + ( 2 * (A * A));
+    int B_2 = ((2 * A) / (B + 1) * (B + 1));
+    int X_2 = B_1 / B_2;
+	printf("O valor da variável X é %d e da X2 é %d ",X_1,X_2);
 }
