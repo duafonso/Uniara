02411c8fd5b3b179e6886b09a2fee6683872315b
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-09-15 15:30:27 -0300

    @

--- Algoritmo e Programação/Atividade 4/Ex.cpp
+++ Algoritmo e Programação/Atividade 4/Ex.cpp
@@ -54,10 +54,11 @@ void Ex3()
 	//	a. O percentual de alunos que utilizaram o restaurante menos de 10x;
 	//	b. O percentual de alunos que utilizaram o restaurante entre 10x e 15x;
 	//	c. O percentual de alunos que utilizaram o restaurante acima de 15x;
-	
 	int quant_mes, cont_alunos=0;
 	int c_a10=0,c_a1015=0,c_a15=0;
-	
+	int cont=5,p_a10,p_a1015,p_a15;
+	while(cont>0){
+	cont--;
 	printf("\n Informe quantas vezes o aluno utilizou o restaurante:");
 	scanf("%d",&quant_mes);
 	cont_alunos++;
@@ -71,15 +72,10 @@ void Ex3()
 	if (quant_mes > 15){
 		c_a15++;
 	}
-	
-	int p_a10 = (c_a10/cont_alunos) * 100;
-	int p_a1015 = (c_a1015/cont_alunos)  * 100;
-	int p_a15 = (c_a15/cont_alunos) * 100;
-	
-	printf("\n %d Aluno(s) utilizaram o restaurante menos de 10 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a10,p_a10,cont_alunos);
-	printf("\n %d Aluno(s) utilizaram o restaurante entre 10 a 15 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a1015,p_a1015,cont_alunos);
-	printf("\n %d Aluno(s) utilizaram o restaurante acima de 15 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a15,p_a15,cont_alunos);
-	
+	}	
+	printf("\n %d Aluno(s) utilizaram o restaurante menos de 10 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a10,p_a10=(c_a10/cont_alunos)*100,cont_alunos);
+	printf("\n %d Aluno(s) utilizaram o restaurante entre 10 a 15 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a1015,p_a1015=(c_a1015/cont_alunos)*100,cont_alunos);
+	printf("\n %d Aluno(s) utilizaram o restaurante acima de 15 vezes no mês. \n Correspondendo a %d por cento do total de alunos pesquisados (%d)",c_a15,p_a15=(c_a15/cont_alunos)*100,cont_alunos);	
 }
 void Ex4()
 {
