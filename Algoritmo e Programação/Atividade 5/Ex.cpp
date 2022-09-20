8e416e655f649e2da0a00c409103a05a8fc26b74
Gabriel Morais <96582619+Gasmorais@users.noreply.github.com>
2022-09-20 12:54:51 -0300

    @

---
+++ Algoritmo e Programação/Atividade 5/Ex.cpp
@@ -0,0 +1,78 @@
+
+void EX1_ALGP_ATV5()
+{
+  // 1. Fez-se uma pesquisa envolvendo um grupo de pessoas. Para cada uma das pessoas,
+  // perguntou-se
+  // A. Valor de seu salário
+  // B. Idade
+  // C. Grau de Instrução (Conforme tabela)
+  // 1 -> Fundamental
+  // 2 -> Médio
+  // 3 -> Superior
+  // Considerando estas informações, faça um algoritmo que forneça:
+  // A. Número de pessoa que possuem curso superior e ganham mais que R$ 5.000,00
+  // B. Diferença entre a média de salários das pessoas com mais de 25 anos e com menos de 25 anos
+  // C. A idade média das pessoas que possuem Ensino Médio
+  // D. O percentual de pessoas que possuem apenas o ensino fundamental
+  // E. A idade média das pessoas com curso superior e a quantidade de pessoas acima desta média
+}
+void EX2_ALGP_ATV5()
+{
+  // 2. Considerando como dados, as notas de uma turma de 40 alunos, escreva um programa
+  // em C que exiba:
+  // A. A média das notas
+  // B. A quantidade de notas acima desta média
+}
+
+void EX3_ALGP_ATV5()
+{
+  // 3. Uma empresa vende N produtos diferentes. A empresa identifica cada produto por um
+  // código alfanumérico formado por 2 letras e 3 números. Dado um vetor com o nome
+  // dos produtos, gere um outro vetor com o código gerado pelo sistema. Exemplo:
+  // A. Arroz Prato Fino -> AP001
+  // B. Arroz Prato Nobre -> AP002
+  // C. Arroz Agulinha -> AG001
+}
+
+void EX4_ALGP_ATV5()
+{
+  // 4. Escreva um algoritmo que preencha um vetor de 50 números inteiros e substitua
+  // todos os números primos por 0 (zero). Imprima o vetor antes e depois da substituição.
+}
+
+void EX5_ALGP_ATV5()
+{
+  // 5. Dado um vetor de 30 números inteiros, exiba:
+  // A. A localização do primeiro número negativo
+  // B. A quantidade de números pares
+  // C. A média dos números ímpares
+  // D. O maior número primo
+}
+
+void EX6_ALGP_ATV5()
+{
+  // 6. Faça um programa que leia um vetor de caractere de 20 posições e inverta a 1º
+  // posição com a da 20º, a 2º com a 19º, e assim por diante. Imprima o vetor antes e depois
+  // da inversão.
+}
+
+void EX7_ALGP_ATV5()
+{
+  // 7. Dada uma sequência de n números, imprimi-la na ordem inversa à da leitura.
+}
+
+void EX8_ALGP_ATV5()
+{
+  // 8. Tentando descobrir se um dado era viciado, um dono de cassino honesto o lançou N
+  // vezes. Dados os N resultados dos lançamentos, determinar o número de ocorrências
+  // de cada face.
+}
+
+void EX9_ALGP_ATV5()
+{
+   // 9. Dada uma sequência de N números reais, determinar os números que compõesm a
+   // sequência e o número de vezes que cada um deles ocorre na mesma.
+   // A. Exemplo: N = 8
+   // Sequência: -1.7, 3.0, 0.0, 1.5, 0.0, -1,7, 2.3, -1,7
+   // Saída: 3,0 ocorre 1 vez, 0.0 ocorre 2 vezes, 1.5 ocorre 1 vez
+}
